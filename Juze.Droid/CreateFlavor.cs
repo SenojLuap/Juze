1150ca0037ad5394bb6fdba952b8c49591385d56
Paul Jones <shirlypunk@hotmail.com>
2017-06-24 15:43:24 -0400

    Added 'Create Flavor' Activity (Unfinished)

---
+++ Juze.Droid/CreateFlavor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Com.Lilarcor.Cheeseknife;
+using Newtonsoft.Json;
+
+namespace paujo.juze.android {
+  [Activity(Label = "@+string/CreateFlavor", ParentActivity = typeof(MainActivity))]
+  public class CreateFlavor : Activity {
+
+    [InjectView(Resource.Id.cfNameField)]
+    public TextView nameField;
+
+    [InjectView(Resource.Id.cfPGBtn)]
+    public CheckBox pgBtn;
+
+    protected override void OnCreate(Bundle savedInstanceState) {
+      base.OnCreate(savedInstanceState);
+
+      SetContentView(Resource.Layout.CreateFlavor);
+      Cheeseknife.Inject(this);
+
+      Flavor flav = JsonConvert.DeserializeObject<Flavor>(Intent.GetStringExtra(Constants.FLAVOR_TYPE_KEY));
+      nameField.Text = flav.Name;
+      pgBtn.Checked = flav.PG;
+    }
+
+
+    /// <summary>
+    /// Called when the 'Create Flavor' button is clicked.
+    /// </summary>
+    /// <param name="caller">The button that triggered the event.</param>
+    /// <param name="args">Extra arguments about the event.</param>
+    [InjectOnClick(Resource.Id.cfCreateBtn)]
+    public void OnCreateClick(Object caller, EventArgs args) {
+      Intent resultIntent = new Intent();
+      Flavor toReturn = CollectFlavorData();
+      string payload = JsonConvert.SerializeObject(toReturn);
+      resultIntent.PutExtra(Constants.FLAVOR_TYPE_KEY, payload);
+      SetResult(Result.Ok, resultIntent);
+      Finish();
+    }
+
+
+    /// <summary>
+    /// Called to collect data from the fields to build a Flavor.
+    /// </summary>
+    /// <returns>A Flavor built from the fields.</returns>
+    public Flavor CollectFlavorData() {
+      Flavor res = new Flavor();
+      res.Name = nameField.Text;
+      res.PG = pgBtn.Checked;
+      return res;
+    }
+  }
+}
---
+++ Juze.Droid/MainActivity.cs
@@ -0,0 +1,74 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using System;
+
+using Com.Lilarcor.Cheeseknife;
+using Android.Content;
+using Newtonsoft.Json;
+
+namespace paujo.juze.android {
+  [Activity(Label = "@+string/JuzeName", MainLauncher = true, Icon = "@drawable/icon")]
+  public class MainActivity : Activity {
+
+    /// <summary>
+    /// Request code for creating a new flavor.
+    /// </summary>
+    public const int CREATE_FLAVOR_REQUEST = 1;
+
+
+    /// <summary>
+    /// On creation of the activity.
+    /// </summary>
+    /// <param name="bundle">The state of the activity, if it is being restored.</param>
+    protected override void OnCreate(Bundle bundle) {
+      base.OnCreate(bundle);
+
+      // Set our view from the "main" layout resource
+      SetContentView(Resource.Layout.Main);
+
+      Cheeseknife.Inject(this);
+    }
+
+
+    /// <summary>
+    /// OnClick for "Create Flavor" button.
+    /// </summary>
+    /// <param name="caller">The button that triggered the callback.</param>
+    /// <param name="args">Information about the click event.</param>
+    [InjectOnClick(Resource.Id.mCreateFlavorBtn)]
+    public void CreateFlavorClick(Object caller, EventArgs args) {
+
+      //Toast.MakeText(ApplicationContext, "Clicked: " + args.GetType().ToString(), ToastLength.Short).Show();
+      Intent flavIntent = new Intent(this, typeof(CreateFlavor));
+
+      Flavor testFlav = new Flavor();
+      testFlav.Name = "CAP Sweet Cream";
+      testFlav.PG = true;
+      testFlav.RecommendedPercentage = 5.0f;
+      testFlav.ID = 1;
+
+      string payload = JsonConvert.SerializeObject(testFlav);
+      flavIntent.PutExtra(Constants.FLAVOR_TYPE_KEY, payload);
+
+      StartActivityForResult(flavIntent, CREATE_FLAVOR_REQUEST);
+    }
+
+    /// <summary>
+    /// Called when an activity returns with a result.
+    /// </summary>
+    /// <param name="requestCode">The code passed to the activity on start.</param>
+    /// <param name="resultCode">The type of result returned.</param>
+    /// <param name="data">The result payload.</param>
+    protected override void OnActivityResult(int requestCode, Result resultCode, Intent data) {
+      if (requestCode == CREATE_FLAVOR_REQUEST) {
+        if (resultCode == Result.Ok) {
+          Flavor resultFlavor = JsonConvert.DeserializeObject<Flavor>(data.GetStringExtra(Constants.FLAVOR_TYPE_KEY));
+          Toast.MakeText(ApplicationContext, "Created: " + resultFlavor.Name + " (" + (resultFlavor.PG ? "PG)" : "VG)"), ToastLength.Short).Show();
+        }
+      }
+    }
+  }
+}
+
